7ea556bd13f71ab90aa05f5c259d190bbbaa14dd
Matias Merenmies <matias.merenmies@gmail.com>
2023-10-26 20:52:22 +0300

    New script to account for day change.

--- night_light.py
+++ night_light.py
@@ -1,31 +1,44 @@
+
 from gpiozero import LED
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
 
-# Connector positions on board
+# Initialize LEDs
 green = LED(17)
 yellow = LED(27)
 red = LED(22)
 
-# Loop to check time and what light should be on
+# Define the time intervals for each LED color
+GREEN_START_TIME = datetime.strptime("06:09", "%H:%M").time()
+GREEN_END_TIME = datetime.strptime("19:09", "%H:%M").time()
+
+RED_START_TIME = datetime.strptime("19:10", "%H:%M").time()
+RED_END_TIME = datetime.strptime("05:54", "%H:%M").time()
+
+YELLOW_START_TIME = datetime.strptime("05:55", "%H:%M").time()
+YELLOW_END_TIME = GREEN_START_TIME  # Using the start of the green period as the end of yellow period for clarity
+
 while True:
     current_time = datetime.now().time()
-    print(current_time)
 
-    if current_time <= datetime.strptime("06:15:00", "%H:%M:%S").time() and current_time > datetime.strptime("19:10:00", "%H:%M:%S").time():
+    # Check the times based on the defined intervals
+    if RED_START_TIME <= current_time or current_time <= RED_END_TIME:  # This condition handles the time wrapping midnight for red
         green.off()
         yellow.off()
         red.on()
-        print('red on')
-    elif current_time >= datetime.strptime("06:00:00", "%H:%M:%S").time() and current_time < datetime.strptime("06:15:00", "%H:%M:%S").time():
+        print('Red is on')
+    elif YELLOW_START_TIME <= current_time < YELLOW_END_TIME:
         green.off()
         yellow.on()
         red.off()
-        print('yellow on')
-    else:
+        print('Yellow is on')
+    elif GREEN_START_TIME <= current_time < GREEN_END_TIME:
         green.on()
         yellow.off()
         red.off()
-        print('green on')
+        print('Green is on')
 
-    time.sleep(60)  # Delay the loop for 60 secodns
+    time.sleep(60)  # Check every minute
+
+
+This setup provides a clear representation of each color interval and makes it easy to adjust the times as needed. The script checks the current time every minute and switches the LEDs based on the defined intervals.
